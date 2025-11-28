911033f8fabc6d1c241408c1923579d0dc15e244
KimTyun <106860407+KimTyun@users.noreply.github.com>
2025-11-28 19:57:06 +0900

    [feat] admin/user의 qna/auth api문서 작성 완료
    Introduced Swagger documentation for admin authentication and QnA APIs by adding admin/authRouteApi.js and admin/qnaRouteApi.js. Added user QnA Swagger docs in qnaRouteApi.js. Updated authRouteApi.js with more detailed request/response schemas and improved descriptions. Modified index.js to register new Swagger paths and tags. Removed .gitkeep files from routes_swagger directories.

--- src/routes_swagger/index.js
+++ src/routes_swagger/index.js
@@ -3,11 +3,26 @@
  * tags:
  *   - name: Auth
  *     description: 인증 관련 API
+ *   - name: QnA
+ *     description: QnA 관련 API
+ *   - name: Admin Auth
+ *     description: 관리자 인증 관련 API
+ *   - name: Admin QnA
+ *     description: 관리자 QnA 관련 API
  */
 
 // Auth API 문서 import
 import { authPaths } from './authRouteApi.js'
+// QnA API 문서 import
+import { qnaPaths } from './qnaRouteApi.js'
+// Admin Auth API 문서 import
+import { adminAuthPaths } from './admin/authRouteApi.js'
+// Admin QnA API 문서 import
+import { adminQnaPaths } from './admin/qnaRouteApi.js'
 
 export const swaggerPaths = {
    ...authPaths,
+   ...qnaPaths,
+   ...adminAuthPaths,
+   ...adminQnaPaths,
 }
