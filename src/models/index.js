c9ebf8452edfd27906db1569254fa42c4a308cfe
SweetYun95 <97324488+SweetYun95@users.noreply.github.com>
2026-01-09 23:32:54 +0900

    [feat]댓글/reply 같은 모델 추가 및 스냅샷 모델 등록

--- src/models/index.js
+++ src/models/index.js
@@ -1,84 +1,115 @@
 // moovy-api/src/models/index.js
-import dotenv from "dotenv";
-import { Sequelize } from "sequelize";
-import cfg from "../config/config.js";
+import dotenv from 'dotenv'
+import { Sequelize } from 'sequelize'
+import cfg from '../config/config.js'
 
-// 각 모델 (기본 포맷)
+/* =========================
+ * User / Auth
+ * ========================= */
+import User from './user.js'
+import UserDetail from './user_detail.js'
+import UserSanction from './user_sanction.js'
 import AdminUser from './admin_user.js'
-import CommentReply from './comment_reply.js'
-import CommentReplyReport from './comment_reply_report.js'
+import PasswordResetToken from './password_reset_token.js'
+
+/* =========================
+ * Video / Content
+ * ========================= */
+import VideoContent from './video_content.js'
+import VideoContentImage from './video_content_image.js'
+import PopularMovieSnapshot from './popular_movie_snapshot.js'
+
+/* =========================
+ * Topic / Comment
+ * ========================= */
+import Topic from './topic.js'
 import CommentTbl from './comment_tbl.js'
+import CommentReply from './comment_reply.js'
 import CommentReport from './comment_report.js'
-import Faq from './faq.js'
-import FaqImage from './faq_image.js'
+import CommentReplyReport from './comment_reply_report.js'
+
+/* =========================
+ * Like / Rating
+ * ========================= */
 import Favorite from './favorite.js'
+import CommentLike from './comment_like.js'
+import ReplyLike from './reply_like.js'
+import Rating from './rating.js'
+
+/* =========================
+ * Board / Support
+ * ========================= */
 import Notice from './notice.js'
 import NoticeImage from './notice_image.js'
 import Qna from './qna.js'
 import QnaImage from './qna_image.js'
-import Rating from './rating.js'
+import Faq from './faq.js'
+import FaqImage from './faq_image.js'
+
+/* =========================
+ * System
+ * ========================= */
 import SystemKeyValue from './system_keyvalue.js'
-import Topic from './topic.js'
-import User from './user.js'
-import UserDetail from './user_detail.js'
-import UserSanction from './user_sanction.js'
-import VideoContent from './video_content.js'
-import VideoContentImage from './video_content_image.js'
 
-// ✅ 신규 추가: 비밀번호 재설정 토큰
-import PasswordResetToken from './password_reset_token.js'
+dotenv.config()
+const env = process.env.NODE_ENV || 'development'
+const config = cfg[env]
 
-dotenv.config();
-const env = process.env.NODE_ENV || "development";
-const config = cfg[env];
-
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    ...config,
-    // logging: false, // ✅ SQL 로그 출력 끄기
-  }
-);
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+   ...config,
+   // logging: false,
+})
 
 const db = {
    sequelize,
    Sequelize,
+
+   // User / Auth
+   User,
+   UserDetail,
+   UserSanction,
    AdminUser,
-   CommentReply,
-   CommentReplyReport,
+   PasswordResetToken,
+
+   // Video / Content
+   VideoContent,
+   VideoContentImage,
+   PopularMovieSnapshot,
+
+   // Topic / Comment
+   Topic,
    CommentTbl,
+   CommentReply,
    CommentReport,
-   Faq,
-   FaqImage,
+   CommentReplyReport,
+
+   // Like / Rating
    Favorite,
+   CommentLike,
+   ReplyLike,
+   Rating,
+
+   // Board / Support
    Notice,
    NoticeImage,
    Qna,
    QnaImage,
-   Rating,
-   SystemKeyValue,
-   Topic,
-   User,
-   UserDetail,
-   UserSanction,
-   VideoContent,
-   VideoContentImage,
+   Faq,
+   FaqImage,
 
-   // ✅ 신규 모델 등록
-   PasswordResetToken,
+   // System
+   SystemKeyValue,
 }
 
 // --- Initialize all models ---
 Object.values(db)
-  .filter((model) => typeof model.init === "function")
-  .forEach((model) => model.init(sequelize));
+   .filter((model) => typeof model.init === 'function')
+   .forEach((model) => model.init(sequelize))
 
 // --- Associate all models ---
 Object.values(db)
-  .filter((model) => typeof model.associate === "function")
-  .forEach((model) => model.associate(db));
+   .filter((model) => typeof model.associate === 'function')
+   .forEach((model) => model.associate(db))
 
-export default db;
-export { sequelize, Sequelize };
+export default db
+export { sequelize, Sequelize }
